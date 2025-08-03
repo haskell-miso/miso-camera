6032b91c679f822409ec020d162c839725b7f1c8
dmjio <code@dmj.io>
2025-08-03 02:48:30 -0500

    Initial commit of miso-camera

---
+++ app/Main.hs
@@ -0,0 +1,78 @@
+----------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE CPP               #-}
+----------------------------------------------------------------------------
+module Main where
+----------------------------------------------------------------------------
+import           Miso hiding ((<#))
+import           Miso.Media
+import           Miso.String (MisoString)
+import qualified Miso.Style as Style
+----------------------------------------------------------------------------
+import           Data.Function
+import           Language.Javascript.JSaddle
+import qualified Data.Map.Strict as M
+----------------------------------------------------------------------------
+-- | Component model state
+data Action
+  = OpenWebCam
+  | OpenedWebCam Stream
+  | ErrorWebCam JSVal
+----------------------------------------------------------------------------
+-- | Entry point for a miso application
+main :: IO ()
+main = run $ startApp app
+  { events = M.insert "click" False mediaEvents
+  }
+----------------------------------------------------------------------------
+-- | WASM export, required when compiling w/ the WASM backend.
+#ifdef WASM
+foreign export javascript "hs_start" main :: IO ()
+#endif
+----------------------------------------------------------------------------
+type Model = ()
+----------------------------------------------------------------------------
+-- | `component` takes as arguments the initial model, update function, view function
+app :: App Model Action
+app = component () update_ $ \() ->
+  div_
+  [ Style.style_
+    [ Style.width (Style.px 500)
+    , Style.height (Style.px 400)
+    , Style.border "2px solid black"
+    ]
+  ]
+  [ h2_ [] [ "🍜 miso-camera 📷" ]
+  , video_
+    [ id_ "vid"
+    , muted_ True
+    , Style.style_
+      [ Style.width (Style.px 500)
+      , Style.height (Style.px 400)
+      , "object-fit" =: "cover"
+      ]
+    ]
+    []
+  , button_
+    [ id_ "button"
+    , autoplay_ True
+    , onClick OpenWebCam 
+    ]
+    [ "Open WebCam"
+    ]
+  ] where
+     update_ :: Action -> Effect Model Action
+     update_ = \case
+       OpenWebCam ->
+         getUserMedia userMedia OpenedWebCam ErrorWebCam
+       OpenedWebCam stream ->
+         io_ $ do
+           vid <- getElementById "vid"
+           vid <# ("srcObject" :: MisoString) $ stream
+           Media vid & play
+       ErrorWebCam errorValue ->
+         io_ $ do
+           consoleLog "Error opening webcam"
+           consoleLog' errorValue
+----------------------------------------------------------------------------
